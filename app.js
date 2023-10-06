066c48cf33d3bd49fb0abeedbd3893757b8019e0
Simran Yadav <simranyadav464@gmail.com>
2023-10-07 01:51:25 +0530

    Updated the files

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 import express from "express";
 import fetch from "node-fetch";
-import _ from "lodash"; 
+import axios from "axios";
+import _ from "lodash";
 const app = express();
-const port = 8000; 
+const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const url = "https://intent-kit-16.hasura.app/api/rest/blogs";
+const headers = {
+  "x-hasura-admin-secret":
+    "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6",
+};
 // Middleware to fetch and analyze blog stats
 app.get("/api/blog-stats", async (req, res) => {
   try {
     // Use fetch to get blog data from the third-party API
-    const response = await fetch(
-      "https://intent-kit-16.hasura.app/api/rest/blogs",
-      {
-        headers: {
-          'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
+    axios
+      .get(url, { headers })
+      .then(async (response) => {
+        if (response.status === 200) {
+          console.log("Website Content:");
+          // console.log(response.data);
+
+          // Data processing should be done here
+          const data = response.data; // Access JSON data directly
+
+          // Perform data analysis using Lodash
+          const totalBlogs = data.length;
+          const longestTitleBlog = _.maxBy(data, (blog) =>
+            blog.title ? blog.title.length : 0
+          );
+          const privacyWordCount = _.filter(
+            data,
+            (blog) => blog.title && blog.title.toLowerCase().includes("privacy")
+          );
+          const uniqueBlogTitles = _.uniqBy(data, "title");
+
+          // Prepare and return the analytics results as JSON response
+          const analyticsResults = {
+            totalBlogs,
+            longestBlogTitle: longestTitleBlog.title,
+            blogsWithPrivacyTitle: privacyWordCount.length,
+            uniqueBlogTitles: uniqueBlogTitles.map((blog) => blog.title),
+          };
+
+          res.json(analyticsResults);
+        } else {
+          console.error(`HTTP error! Status: ${response.status}`);
+          res
+            .status(response.status)
+            .json({ error: `HTTP error! Status: ${response.status}` });
         }
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from the API");
-    }
-
-    const data = await response.json();
-
-    // Perform data analysis using Lodash
-    const totalBlogs = data.length;
-    const longestTitleBlog = _.maxBy(data, "title.length");
-    const privacyWordCount = _.filter(data, (blog) =>
-      _.includes(blog.title.toLowerCase(), "privacy")
-    );
-    const uniqueBlogTitles = _.uniqBy(data, "title");
-
-    // Prepare and return the analytics results as JSON response
-    const analyticsResults = {
-      totalBlogs,
-      longestBlogTitle: longestTitleBlog.title,
-      blogsWithPrivacyTitle: privacyWordCount.length,
-      uniqueBlogTitles: uniqueBlogTitles.map((blog) => blog.title),
-    };
-
-    res.json(analyticsResults);
+      })
+      .catch((error) => {
+        console.error("Request error:", error);
+        res.status(500).json({ error: "Internal server error" });
+      });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
@@ -52,7 +66,7 @@ app.get("/api/blog-stats", async (req, res) => {
 // Create a route for blog search
 app.get("/api/blog-search", async (req, res) => {
   try {
-    const query = req.query.query; 
+    const query = req.query.query;
 
     if (!query) {
       return res.status(400).json({ error: "Missing query parameter" });
@@ -71,14 +85,18 @@ app.get("/api/blog-search", async (req, res) => {
     const data = await response.json();
 
     // Implement custom search functionality
-    const searchResults = data.filter((blog) =>
-      blog.title.toLowerCase().includes(query.toLowerCase())
-    );
+    const searchResults = data.filter((blog) => {
+      if (!blog || typeof blog.title !== "string") {
+        console.warn("Skipping invalid blog:", blog);
+        return false;
+      }
+      return blog.title.toLowerCase().includes(query.toLowerCase());
+    });
 
     // Send the search results as a JSON response
     res.json(searchResults);
   } catch (error) {
-    console.error(error);
+    console.error("Error during blog search:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
@@ -88,7 +106,6 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: "Internal server error" });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
